6bc072984216b153a9d3a25637e1e9f4604fcbfe
Olha <olusiabn@gmail.com>
2023-05-16 15:39:50 +0200

    Add change password form and page

--- WebChatProject/chat/urls.py
+++ WebChatProject/chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import PasswordChangeView
+from . import views
 
 urlpatterns = [
     # chat
@@ -8,7 +9,12 @@ urlpatterns = [
     path("chat/<str:room_name>/", views.room, name="room"),
 
     # login, logout
-    # http://127.0.0.1:8000/auth/login
+    # http://127.0.0.1:8000/auth/login/
     path("auth/login/", LoginView.as_view(template_name="chat/loginPage.html"), name="login_user"),
     path("auth/logout/", LogoutView.as_view(), name="logout_user"),
+
+    # change password
+    # http://127.0.0.1:8000/auth/change_password/
+    path('auth/change_password/', PasswordChangeView.as_view(template_name='chat/change_password.html',
+                                                             success_url='/'), name='change_password'),
 ]
